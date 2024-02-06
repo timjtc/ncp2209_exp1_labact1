3ae7291c09eeed6abead48782a42032601601a8f
Timothy Concepcion <timothyjtc@outlook.com>
2024-02-06 08:47:33 +0800

    first commit

---
+++ exp1/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void main(int argc, char *arg[])
+
+{
+	int pid; pid = fork();
+	if (pid < 0)
+	{
+		printf("fork failed");
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		execlp("whoami", "1s", NULL);
+		exit(0);
+	}
+	else
+	{
+		printf("\n Process ID is: %d\n", getpid());
+		wait(NULL);
+		exit(0);
+   }
+}
---
+++ lab1cs/ncp2209_lab1cs/NCP2209_Lab1Cs.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+class NCP2209_Lab1Cs
+{
+    static void Main(string[] args)
+    {
+        // Create a new process
+        Process fork;
+
+        try
+        {
+            // Start the process
+            fork = execProcess("cmd", "/C whoami");
+
+            // Print the process ID of both parent and child
+            Console.WriteLine($"Parent PID is {Environment.ProcessId}");
+            Console.WriteLine($"Child PID is {fork.Id}");
+
+            // Wait for the process to finish
+            fork.WaitForExit();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Failed to start fork! " + e.Message);
+        }
+    }
+    
+    private static Process execProcess(string command, string args)
+    {
+        Process p = new Process();
+        p.StartInfo = new ProcessStartInfo(command, args);
+        p.Start();
+
+        return p;
+    }
+}
